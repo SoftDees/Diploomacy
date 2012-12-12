c80d6c3335f4923fa8635cdad8c95d598bde316e
Shane Skikne <sskikne@Shane-Skikne.(none)>
2012-12-12 03:33:31 -0500

    New Game start

--- Game_Start.py
+++ Game_Start.py
@@ -1,19 +1,20 @@
+
 from country import *
 from world_functions import *
 from make_map import *
-ACL = [["SAC", "A"], ["MAC", "A"], ["CAC", "F"], ["MCR", "A"]]
+ACL = {"SAC": "A", "MAC": "A", "CAC": "F", "MCR": "A"}
 ACS = ["SAC", "MAC", "CAC", "MCR"]
 
-CCL = [["LDK", "F"],["FAC", "A"],["DIH", "A"]]
+CCL = {"LDK": "F","FAC": "A","DIH": "A"}
 CCS = ["LDK", "FAC", "DIH"]
 
-MHL = [["LIB", "A"],["MEZ", "A"],["ADM", "F"]]
+MHL = {"LIB": "A","MEZ": "A","ADM": "F"}
 MHS = ["LIB","MEZ","ADM"]
 
-WHL = [["WHN", "A" ], ["WHW","F"], ["WHE","A"]]
+WHL = {"WHN": "A" , "EWF":"F", "WHE":"A"}
 WHS = ["WHN", "WHW", "WHE"]
 
-EHL = [["EHN", "A"],["EHW","A"],["EHE","F"]]
+EHL = {"EHN": "A","EHW":"A","EHE":"F"}
 EHS = ["EHN", "EHW", "EHE"]
 
 #Locations, supply, Name, usrname
@@ -26,7 +27,6 @@ EH = Country(EHL, EHS, "EHPlayer", "EH", "Purple")
 countries = [AC,CC,MH, WH, EH]
 wo = world(countries)
 update_map(wo.countries, "BaseMap.png")
-EH.move(loc, dest, wo)
-EH.support(loc, attacked, attacking, wo)
-
+#EH.move(loc, dest, wo)
+#EH.support(loc, attacked, attacking, wo)
 
